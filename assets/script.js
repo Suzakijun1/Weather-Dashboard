e1e61bff9d7469c2096090fc6da56a2822cfe045
suzakijun1 <dyong1997@gmail.com>
2023-04-08 18:56:03 -0400

    fixing the html and css

--- assets/script.js
+++ assets/script.js
@@ -24,27 +24,38 @@ function fetchForecast(city) {
   // $("#Temp0").html(response.main.temp);
   //console.log(response.main.temp);
 }
+const futureWeather = document.querySelector("#future-weather");
 
 function renderForecast(forecastData) {
-  $("#future-weather").html(
-    `<div class = "col-sm-2 big-primary forecast">
-                <p id="date0"></p>
-                herouwegroiwageup9eg
-                <p id="Img0"></p>
-                <p>lkjwgeojigweapoi: <span id="Temp0"></span></p>
-                <p>Humidity: <span id="humidity0">hello world</span></p>`
-  );
-  console.log(forecastData.list[0].temp.day);
+  futureWeather.html = ``;
+  for (var i = 1; i <= 5; i++) {
+    //create a div 'forecastCard'
+    const forecastCard = document.createElement("div");
+    forecastCard.innerHTML = `<div class = "col-sm-2 big-primary forecast">
+                  <p id="date0">${new Date(
+                    forecastData.list[i].dt * 1000
+                  ).toLocaleDateString()}</p>
+                  <img src="https://openweathermap.org/img/wn/${
+                    forecastData.list[i].weather[0].icon
+                  }@2x.png"/>
+                  <p>Temp: ${forecastData.list[i].temp.day}</p>
+                  <p>Humidity: ${forecastData.list[i].humidity}</p>`;
+
+    futureWeather.append(forecastCard);
+  }
+
+  //append forecast card to future-weather
+  console.log(forecastData.list[1]);
 }
 
-function futureWeather(data) {
-  //const name = data;
-  //const { icon, description } = data.weather[0];
-  //const temp = list[0].temp.day;
-  //const speed = data.wind;
-  // $("#Temp0").html(temp);
-  console.log("future weather");
-}
-futureWeather();
+//function futureWeather(data) {
+//const name = data;
+//const { icon, description } = data.weather[0];
+//const temp = list[0].temp.day;
+//const speed = data.wind;
+// $("#Temp0").html(temp);
+// console.log("future weather");
+//}
+//futureWeather();
 
 $("#search-button").on("click", fetchForecast);
